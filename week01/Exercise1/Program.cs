b97dcf5c15ce23118420536ac4e39ab1aa068863
godwino17 <ginyang3@byupathway.edu>
2026-01-07 12:07:35 +0100

    Wk01 C# Programming Exercise 1 - Variable, Input, Output

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -11,7 +11,6 @@ class Program
         Console.Write("What is your last name? ");
         string last_name= Console.ReadLine();
 
-        // Console.WriteLine();
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
          
     }
