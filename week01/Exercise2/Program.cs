ab1965633c1427a51f5e2fa30479a3bba40268ff
godwino17 <ginyang3@byupathway.edu>
2026-01-07 12:21:00 +0100

    WK01 C# Programming Exercise 2 - Conditionals Sttatement

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,83 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string grade= Console.ReadLine();
+        float score= float.Parse(grade);
+
+        string sign= "";
+
+        string letter= "";
+        float scoreDiv= score % 10;
+
+
+
+        if (score >= 90)
+        {
+            if (scoreDiv <= 3)
+            {
+                sign = "-";
+            }
+            letter= "A";
+        }
+        else if (score >= 80)
+        {
+            if (scoreDiv <= 3)
+            {
+                sign = "-";
+            }
+            else if (scoreDiv >= 7)
+            {
+                sign = "+";
+            }
+            letter= "B";
+        }
+        else if (score >= 70)
+        {
+            if (scoreDiv <= 3)
+            {
+                sign = "-";
+            }
+            else if (scoreDiv >= 7)
+            {
+                sign = "+";
+            }
+            letter= "C";
+        }
+        else if (score >= 60)
+        {
+            if (scoreDiv <= 3)
+            {
+                sign = "-";
+            }
+            else if (scoreDiv >= 7)
+            {
+                sign = "+";
+            }
+            letter= "D";
+        }
+        else
+        {
+            letter= "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        
+
+
+        if (score >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time.");
+        }
+
     }
 }
