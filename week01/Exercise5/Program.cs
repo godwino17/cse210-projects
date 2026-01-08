e75461267b4f95b2261f708c188af82851b6e6ab
godwino17 <ginyang3@byupathway.edu>
2026-01-08 11:56:13 +0100

    WK01 C# Programming Exercise 5 - Functions✅

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+
+        }
+    
+        DisplayWelcome();
+
+        static string PromtUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name= Console.ReadLine();
+            return name;
+        }
+
+        static int PromtUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string nums= Console.ReadLine();
+            int number= int.Parse(nums);
+            return number;
+        }
+
+        static int SquareNumber(int num)
+        {
+            int SquareNum= num * num;
+            return SquareNum;
+        }
+
+        static string DisplayResult(string userName, int SqrNum)
+        {
+            return $"{userName}, the square of your number is {SqrNum}";
+        }
+
+        static void main()
+        {
+            string name= PromtUserName();
+            int number= PromtUserNumber();
+            int square= SquareNumber(number);
+            string displayDetails= DisplayResult(name, square);
+            Console.WriteLine(displayDetails);
+        }
+
+        main();
     }
 }
